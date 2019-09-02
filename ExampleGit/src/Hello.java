8269255629950b38b611db694a6839ebbd27f541
tcs.user22 <tcs.user22@LBCOITLELT8551.LandMarkGroup.India>
2019-09-02 14:20:08 +0530

    Third commit

--- ExampleGit/src/Hello.java
+++ ExampleGit/src/Hello.java
@@ -5,8 +5,13 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		int x=10;
+		int y=20;
+		int z=x+y;
 		System.out.println("hello world");
 		System.out.println("this is second line");
+		System.out.println("the value of x and y:"+z);
 
 	}
 
